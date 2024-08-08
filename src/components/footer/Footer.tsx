ec3414a4f837a8bb7d068416a5f9b82be6784b11
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-08-08 09:20:02 -0300

    feature: atualizando footer e instalando dependencias axiso/spinner

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import './Footer.css'
 
 function Footer() {
  
@@ -11,10 +12,10 @@ function Footer() {
           <div className="container flex flex-col items-center py-4">
             <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
-            <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
-              <InstagramLogo size={48} weight='bold' />
-              <FacebookLogo size={48} weight='bold' />
+            <div className='flex gap-10'>
+              <LinkedinLogo size={48} weight='bold' id='logo' />
+              <InstagramLogo size={48} weight='bold' id='logo' />
+              <FacebookLogo size={48} weight='bold' id='logo' />
             </div>
           </div>
         </div>
