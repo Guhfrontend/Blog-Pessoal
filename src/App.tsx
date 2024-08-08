8534dbf44a7cd2a4d7e15b509683f5eb067a91b6
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-08-08 11:02:52 -0300

    feat: UserContext reafctor: login,home,app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Login from "./pages/Login/Login"
-import Home from "./pages/Home/Home"
-import Navbar from "./components/navBar/NavBar"
-import Footer from "./components/footer/Footer"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import Navbar from "./components/navBar/NavBar";
+import Footer from "./components/footer/Footer";
+import axios from "axios";
+import { Users } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
 
 function App() {
+  
+  interface User {
+    id: number;
+    name: string;
+  }
 
+const [users, setUsers] = useState<User[]>([]);
+  
+    useEffect(() => {
+      axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+          setUsers(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
 
   return (
 
     <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
+      <Navbar />
+      <div className="min-h-[80vh]">
 
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="login" element={<Login />}/>
-        <Route path="home" element={<Home />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="home" element={<Home />} />
+        </Routes>
 
       </div>
       <Footer />
