cde42fab7a3f45d9d30bf093481504df3f8b6dcf
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-08-09 14:37:02 -0300

    criando template e logica dos temas

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Login from './pages/Login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/Home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 
 function App() {
@@ -17,11 +20,15 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
-            <Routes>
+          <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
